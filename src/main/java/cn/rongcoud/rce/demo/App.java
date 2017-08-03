4d46e27cbb3383ce1546b2faf79b43e79af21974
cuizhennan <cuizhennan@rongcloud.cn>
2017-08-03 15:56:34 +0800

    test git rebase

--- src/main/java/cn/rongcoud/rce/demo/App.java
+++ src/main/java/cn/rongcoud/rce/demo/App.java
@@ -7,5 +7,6 @@ package cn.rongcoud.rce.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("test git");
     }
 }
